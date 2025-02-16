274a58c292ba2c11352cc97aa0fb1fa1372f936a
Mikołaj Szustakowski <55512451+m1k53r@users.noreply.github.com>
2025-02-16 18:27:19 +0100

    Tileset (#3)

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -5,21 +5,21 @@ extends StaticBody2D
 @onready var _prompt: Label = $InteractionPrompt
 
 func _ready() -> void:
-    _prompt.visible = false
+	_prompt.visible = false
 
 
 func enter_interaction(player: Player) -> void:
-    update_prompt(player)
+	update_prompt(player)
 
 
 func exit_interaction(_player: Player) -> void:
-    _prompt.visible = false
+	_prompt.visible = false
 
 
 func interact(player: Player) -> void:
-    player.inventory.pick_up(item)
-    update_prompt(player)
+	player.inventory.pick_up(item)
+	update_prompt(player)
 
 
 func update_prompt(player: Player) -> void:
-    _prompt.visible = not player.inventory.is_full()
+	_prompt.visible = not player.inventory.is_full()
