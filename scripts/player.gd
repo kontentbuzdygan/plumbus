822351e38c143bcef7420734090bff6ac8fdf4a7
m1k53r <mikobw369@protonmail.com>
2025-02-16 15:39:12 +0100

    init

---
+++ scripts/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 150.0
+
+var direction_names = { 
+	Vector2.LEFT: "left",
+	Vector2.RIGHT: "right",
+	Vector2.UP: "up",
+	Vector2.DOWN: "down"
+}
+
+var last_direction := Vector2.DOWN
+
+func _process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED
+
+	move_and_slide()
+	update_animation(direction)
+
+
+func update_animation(direction: Vector2) -> void:
+	match direction:
+		Vector2.ZERO:
+			animated_sprite_2d.play(direction_names.get(last_direction) + "_idle")
+			return
+		Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN:
+			last_direction = direction
+			
+	animated_sprite_2d.play(direction_names.get(last_direction) + "_walk")
