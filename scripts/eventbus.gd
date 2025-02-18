34c9c3976a1fa322d35885ca5557abc8a84f519a
m1k53r <mikobw369@protonmail.com>
2025-02-18 19:29:36 +0100

    add eventbus

---
+++ scripts/eventbus.gd
@@ -0,0 +1,3 @@
+extends Node
+
+signal timer_tick(time)
--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,18 +1,8 @@
 extends Timer
 
-@onready var label: Label = $"../HBoxContainer/CountdownLabel"
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text = str(_format_time(snapped(time_left, 1)))
+	Eventbus.timer_tick.emit(time_left)
 
 
 func _on_timeout() -> void:
 	print("Game over")
-
-
-func _format_time(seconds: int) -> String:
-	var fminutes = str(floor(seconds / 60))
-	var fseconds = str(seconds % 60)
-	fseconds = "0" + fseconds if fseconds.length() == 1 else fseconds
-	return "%s:%s" % [fminutes, fseconds]
