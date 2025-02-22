523674d1c352dc02c57a981bb0d8c208b6c03642
michalwa <michalwa2003@gmail.com>
2025-02-22 13:58:45 +0100

    feat: sliding animations

--- scripts/new_inventory.gd
+++ scripts/new_inventory.gd
@@ -7,13 +7,16 @@ enum ColumnState { TOP, BOTTOM }
 @export var bottom_inventory: Inventory
 @export var icon_size: float = 16
 @export var cell_margin: float = 1
+@export var animation_speed: float = 0.6
 @export var highlight: StyleBox
 
 var items: Array[Item]
 var states: Array[ColumnState]
+var ts: Array[float]
 var selected: int = 0
 var cell_size: float:
     get: return icon_size + cell_margin * 2
+
 
 
 func open() -> void:
@@ -24,10 +27,12 @@ func open() -> void:
     for item in top_inventory.items:
         items.append(item)
         states.append(ColumnState.TOP)
+        ts.append(0.0)
 
     for item in bottom_inventory.items:
         items.append(item)
         states.append(ColumnState.BOTTOM)
+        ts.append(1.0)
 
     size.x = items.size() * cell_size
     size.y = 2 * cell_size
@@ -35,10 +40,16 @@ func open() -> void:
     show()
 
 
+func _process(_delta: float) -> void:
+    queue_redraw()
+
+
 func _draw() -> void:
     for i in range(items.size()):
         var x = i * cell_size
-        var y = 0.0 if states[i] == ColumnState.TOP else cell_size
+        var target_t = 0.0 if states[i] == ColumnState.TOP else 1.0
+        ts[i] = lerp(ts[i], target_t, animation_speed)
+        var y = lerp(0.0, float(cell_size), ts[i])
 
         if i == selected:
             draw_style_box(highlight, Rect2(x, y, cell_size, cell_size))
@@ -67,5 +78,3 @@ func _unhandled_input(event: InputEvent) -> void:
 
     if event.is_action_pressed("ui_right"):
         selected = min(items.size() - 1, selected + 1)
-
-    queue_redraw()
